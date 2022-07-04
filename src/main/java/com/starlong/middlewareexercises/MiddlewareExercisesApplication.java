9dfcd9261b2d6fa5f3db5c3dbb81142e7391ff7c
starLLong <2554868271@qq.com>
2022-07-04 09:23:05 +0800

    Initial commit

---
+++ src/main/java/com/starlong/middlewareexercises/MiddlewareExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.starlong.middlewareexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlong/middlewareexercises/MiddlewareExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlong.middlewareexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
